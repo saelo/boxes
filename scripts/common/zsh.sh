f62d2be2d644135c2e404c5d86a10a600af9558c
Jan Keim <uaeni@student.kit.edu>
2016-01-03 02:22:54 +0100

    standard zshrc will now be loaded

--- scripts/common/zsh.sh
+++ scripts/common/zsh.sh
@@ -3,7 +3,7 @@
 echo "Setting up zsh..."
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh > /dev/null 2>&1
-curl -s https://gist.githubusercontent.com/saelo/7614863612553a4647ae/raw/.zshrc > ~/.zshrc
+curl -s https://raw.githubusercontent.com/Gram21/boxes/master/ctfbox/.zshrc_std > ~/.zshrc
 
 # needs sudo to work passwordless
 sudo chsh -s $(which zsh) $(whoami)
