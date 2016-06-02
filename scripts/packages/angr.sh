4a00d2e0d7e325fc534ff8881488d22db4a074a4
Jan Keim <uaeni@student.kit.edu>
2016-06-02 15:22:38 +0200

    repaired damaged stuff

--- scripts/packages/angr.sh
+++ scripts/packages/angr.sh
@@ -6,7 +6,8 @@
 set -e
 
 # Install dependencies.
-sudo apt-get -y install python2.7-dev build-essential libxml2-dev libxslt1-dev git libffi-dev cmake libreadline-dev
+sudo apt-get -y install virtualenvwrapper python2.7-dev build-essential libxml2-dev libxslt1-dev git libffi-dev cmake libreadline-dev libtool debootstrap debian-archive-keyring libglib2.0-dev libpixman-1-dev
+
 
 # Install angr
 git clone https://github.com/angr/angr-dev .angr
