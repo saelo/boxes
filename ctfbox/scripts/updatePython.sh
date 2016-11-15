3892d079b09b5ff219718612f16731e079b23d88
Jan Keim <uaeni@student.kit.edu>
2016-11-15 18:21:17 +0100

    fix missing numbers in python update script

--- ctfbox/scripts/updatePython.sh
+++ ctfbox/scripts/updatePython.sh
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 echo "### py2 ###"
-pip2 list --outdated | cut -d ' ' -f1 | xargs -n1 sudo -H pip install -U
+pip2 list --outdated | cut -d ' ' -f1 | xargs -n1 sudo -H pip2 install -U
 
 #echo "### py3 ###"
-#pip3 list --outdated | cut -d ' ' -f1 | xargs -n1 sudo -H pip install -U
+#pip3 list --outdated | cut -d ' ' -f1 | xargs -n1 sudo -H pip3 install -U
