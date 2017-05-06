f861fd1bb3a5cb3d6f68d0836ae420522ecab2e0
Samuel Groß <dev@samuel-gross.com>
2017-05-06 10:56:05 +0200

    Now using vim-plug as plugin manager

--- scripts/common/vim.sh
+++ scripts/common/vim.sh
@@ -2,46 +2,11 @@
 
 echo "Setting up vim..."
 
-# Write bootstrap vimrc
-cat > ~/.vimrc <<EOF
-set nocompatible        " be IMproved
-filetype off            " required, reverted below
+# Grab vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-" set the runtime path to include Vundle and initialize
-set rtp+=~/.vim/bundle/Vundle.vim
-call vundle#begin()
-" alternatively, pass a path where Vundle should install plugins
-"call vundle#begin('~/some/path/here')
-
-" let Vundle manage Vundle, required
-Plugin 'VundleVim/Vundle.vim'
-
-"
-" Plugins
-"
-Plugin 'scrooloose/nerdtree'
-Plugin 'scrooloose/nerdcommenter'
-Plugin 'terryma/vim-multiple-cursors'
-Plugin 'airblade/vim-gitgutter'
-Plugin 'vim-airline/vim-airline'
-Plugin 'vim-airline/vim-airline-themes'
-Plugin 'edkolev/tmuxline.vim'
-Plugin 'rking/ag.vim'
-Plugin 'Valloric/YouCompleteMe'
-Plugin 'christoomey/vim-tmux-navigator'
-
-Plugin 'altercation/vim-colors-solarized'
-
-" All of your Plugins must be added before the following line
-call vundle#end()            " required
-filetype plugin indent on    " required
-EOF
-
-# Grab vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# Fetch real vimrc
+# Fetch vimrc
 curl -s https://raw.githubusercontent.com/saelo/dotfiles/master/vimrc > ~/.vimrc
 
 # Install plugins
-vim +PluginInstall +qall > /dev/null
+vim +PlugInstall +qall > /dev/null
